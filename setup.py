a6940d6c3415522bf19cb5590bc205695360a302
gonzalofmora <gonzalofmora@gmail.com>
2025-03-18 20:21:13 -0300

    Bases para el repositorio de automatización de tareas contables

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="estudio_contable",
+    version="0.1",
+    description="automatizaciones para un estudio contable",
+    author="G.M.",
+    packages=find_packages(where="src"),  # Look for packages in the "src" directory
+    package_dir={"": "src"},  # Tell setuptools that packages are under "src"
+    install_requires=[
+        "pandas>=2.2.3"
+    ],
+    python_requires=">=3.12"
+)
