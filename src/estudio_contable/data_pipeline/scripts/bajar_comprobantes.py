9a933722fbe63ce4434715604f92a3cd23f5b44f
gonzalofmora <gonzalofmora@gmail.com>
2025-04-13 23:31:47 -0300

    modificación menor del archivo bajar_comprobantes.
    Ahora todas las variables se manejan desde el .env

--- src/estudio_contable/data_pipeline/scripts/bajar_comprobantes.py
+++ src/estudio_contable/data_pipeline/scripts/bajar_comprobantes.py
@@ -8,6 +8,8 @@ carpeta_de_descarga = Path(variables.get('MC_FOLDER_ORIGIN_PATH'))
 carpeta_de_destino = Path(f"{variables.get('MC_FOLDER_DESTINATION_PATH')}/{tipo_comprobantes.capitalize()}")
 mes =  variables.get('MES')
 tipo_archivo = variables.get('TIPO_ARCHIVO') # 'excel' | 'csv'
+en_cantidad = (variables.get('EN_CANTIDAD') == 'True')
 
 # True para todos los clientes, False para uno solo
-mc_obtener_comprobantes(tipo_archivo, tipo_comprobantes, mes, carpeta_de_descarga, carpeta_de_destino, False)
+mc_obtener_comprobantes(tipo_archivo, tipo_comprobantes, mes, carpeta_de_descarga, carpeta_de_destino, en_cantidad)
+
