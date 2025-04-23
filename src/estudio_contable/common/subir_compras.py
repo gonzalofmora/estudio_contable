2ea10950eb1104891dab604ef546db617c827c1c
gonzalofmora <gonzalofmora@gmail.com>
2025-04-22 23:44:30 -0300

    Modificaciones menores al archivo de subir_compras.py

--- src/estudio_contable/common/subir_compras.py
+++ src/estudio_contable/common/subir_compras.py
@@ -5,6 +5,19 @@ import time
 
 from estudio_contable.common.funciones_pub import afip_login, afip_cargar_compras, elegir_tab, link_afip_compras_portal_iva
 
+"""
+Pasos para subir las compras a AFIP
+
+1° Cambiar la info correspondiente en el archivo .env:
+    - Ruta de acceso al excel (excel_path)
+    - Usuario AFIP (user)
+    - Contraseña de AFIP (password)
+
+2° Ejecutar el archivo (subir_compras.py)
+
+3° Navegar una vez iniciada la sesión hasta el aplicativo Portal IVA y dejar listo para que el archivo empiece a cargar facturas. Confirmar con Enter (falta automatizar)
+"""
+
 variables = load_dotenv()
 excel_path = os.getenv('EXCEL_FILE_PATH')
 compras_a_subir = pd.read_excel(excel_path, sheet_name="Compras_a_subir", dtype={
@@ -13,6 +26,19 @@ compras_a_subir = pd.read_excel(excel_path, sheet_name="Compras_a_subir", dtype=
     "Número Hasta": str,
     "Nro. CUIT. Emisor": str})
 
+number_columns = [
+    'Neto 21%',
+    'Neto 10,5%',
+    'Imp. Neto No Gravado',
+    'Imp. Op. Exentas',
+    'IVA 21%',
+    'IVA 10,5%',
+    'Perc. IIBB',
+    'Perc. IVA',
+    'Imp. Total'
+]
+
+compras_a_subir[number_columns] = compras_a_subir[number_columns].round(2)
 
 user = os.getenv('USER')
 password = os.getenv('PASSWORD')
